fdbbde2bdf5be3341f6917253aa2c7ac89d0247a
friendly-format-0074 <samuelagasi@gmail.com>
2020-12-28 00:10:55 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,22 +5,22 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  answer = "num1 - num2"
+  answer = num1 - num2
   answer
 end
 
 def division(num1, num2)
-  answer = "num1 / num2"
+  answer = num1 / num2
   answer
 end
 
 def multiplication(num1, num2)
-  answer = "num1 * num2"
+  answer = num1 * num2
   answer
 end
 
 def modulo(num1, num2)
-  answer = "num1 % num2"
+  answer = num1 % num2
   answer
 end
 
